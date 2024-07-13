287705c44f1e0ff8e0d2069ceb36ad06f80eebe4
alexisraimbault <27raimbault.alexis@gmail.com>
2024-07-13 16:14:20 +0200

    Feat [view] updated regex in middleware to match param

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from '@kobbleio/next/server'
 
 export default authMiddleware({
-    publicRoutes: ['/', '/view', '/view/', '/view/*'],
+    publicRoutes: ['/', '/view\/(.*)'],
 });
 
 export const config = {
